f12cf787decab9a44e61be4d265fc5832d354ec0
Gerrit Vos <gevo27@gmail.com>
2015-10-25 10:35:17 +0100

    Aanzet main page

--- javascripts/lijsten.js
+++ javascripts/lijsten.js
@@ -1,5 +1,11 @@
 jQuery(document).ready(function($) {
 
+  // Main page sorting
   $('.sortable').sortable();
+
+  // Status
+  $('.overview-items .status ul .arrow').click(function(e) {
+	$('.overview-items .status ul li').show();
+  });
 
 });
