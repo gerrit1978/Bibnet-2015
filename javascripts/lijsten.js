e1038b09f4a897c82c2688df80f2a4051988e153
Gerrit Vos <gevo27@gmail.com>
2015-11-03 11:07:53 +0100

    UI voor status

--- javascripts/lijsten.js
+++ javascripts/lijsten.js
@@ -1,9 +1,9 @@
-jQuery(document).ready(function($) {
-
+$(document).ready(function() {
   // Main page sorting
   $('.sortable').sortable();
 
   // Status
+  /*
   $('.overview-items .status ul .arrow').click(function(e) {
     var list = $(this).parent();
     if (list.hasClass('open')) {
@@ -18,6 +18,7 @@ jQuery(document).ready(function($) {
   $('.overview-items .status ul li a').click(function(e) {
     $(this).parent().parent().parent().parent().parent().find('.message').html('<strong>Vergeet dit item niet te raten!</strong>');
   });
+  */
   
   // Raty
   $('.rating').raty({
@@ -40,6 +41,80 @@ jQuery(document).ready(function($) {
      			}
   });
 
+  /**
+   * List detail page
+   */
 
+  // 1. Iterate over the items for initializing
+  $('ul.list-detail li.overview-item').each(function() {
+    var currentStatus = $(this).find('input[name=status]').val();
+
+    // Show ratings for items that have status "done"
+    if (currentStatus == 'done') {
+      $(this).find('.rating-wrapper').show();
+    }
+    
+    // Hide current status link
+    $(this).find(".status ul li a").each(function() {
+      var target = $(this).attr('data-target');
+      if (target == currentStatus) {
+        $(this).parent("li").hide();
+      }
+    });
+  });
+  
+
+  // 2. Toggle status
+  $('a.toggle-status').click(function(e) {
+    // get the new status
+    var newStatus = $(this).attr('data-target');
+  
+    // get item delta and id
+    var delta = $(this).attr('data-delta');
+    var id = "#item-" + delta;
+    //var currentStatus = $(id).find('input[name=status]').val();
+
+    // set new status in hidden field
+    $(id).find('input[name=status]').val(newStatus);
+    
+    // set new status in frontend and toggle visibility of rating
+    if (newStatus == "todo") {
+      var html = "Te lezen";
+      $(id).find('.rating-wrapper').hide();
+    }
+    if (newStatus == "done") {
+      var html = "Gelezen";
+      $(id).find('.rating-wrapper').show();
+    }
+    $(id).find('li.current-status').html(html);
+    
+    // hide current link
+    var selectorLinks = id + " .status ul li";
+    $(selectorLinks).show();
+    $(this).parent().hide();
+    
+    // adjust height
+    var selectorDiv = id + " .status";
+    $(selectorDiv).height(35).css('border', 'none');
+    
+    /*
+     * Erwin, do AJAX call HERE
+     */
+    e.preventDefault();
+  });
+  
+  // 3. Toggle visibility 
+  $('.overview-items .status ul .arrow').click(function(e) {
+    var div = $(this).parent().parent();
+    var currentHeight = $(div).height();
+    if (currentHeight == 35) {
+		$(div).height(70).css('border', '1px solid white');
+    } else {
+      $(div).height(35).css('border', 'none');
+    }
+    console.log($(this).parent().parent().height());
+    $(this).parent('.status').hide();
+    e.preventDefault();
+  });
 
 });
